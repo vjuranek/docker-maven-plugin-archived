2f829b86727a627b2dcb2eb931ee81c2b49f45df
Vojtech Juranek <vjuranek@redhat.com>
2014-02-25 23:11:13 +0100

    Allow multiple container ids, add default phase

--- src/main/java/cz/jurankovi/docker/maven/ContainerStartMojo.java
+++ src/main/java/cz/jurankovi/docker/maven/ContainerStartMojo.java
@@ -1,20 +1,23 @@
 package cz.jurankovi.docker.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import com.kpelykh.docker.client.DockerException;
 
-@Mojo(name = "container-start")
+@Mojo(name = "container-start", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
 public class ContainerStartMojo extends AbstractDockerMojo {
 
     @Parameter(required = true)
-    private String containerId;
+    private String[] containerIds;
 
     public void execute() throws MojoExecutionException {
         try {
-            getClient().startContainer(containerId);
+            for(String id : containerIds) {
+                getClient().startContainer(id);
+            }
         } catch (DockerException e) {
             // TODO log
             throw new MojoExecutionException(e.getMessage());
--- src/main/java/cz/jurankovi/docker/maven/ContainerStopMojo.java
+++ src/main/java/cz/jurankovi/docker/maven/ContainerStopMojo.java
@@ -1,20 +1,23 @@
 package cz.jurankovi.docker.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import com.kpelykh.docker.client.DockerException;
 
-@Mojo(name="container-stop")
+@Mojo(name="container-stop", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST)
 public class ContainerStopMojo extends AbstractDockerMojo {
 
     @Parameter(required=true)
-    private String containerId;
+    private String[] containerIds;
 
     public void execute() throws MojoExecutionException {
         try {
-            getClient().stopContainer(containerId);
+            for(String id : containerIds) {
+                getClient().stopContainer(id);
+            }
         } catch (DockerException e) {
             // TODO log
             throw new MojoExecutionException(e.getMessage());
