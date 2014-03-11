26bfe8115b063e08923be4037def7a3f96c8dad5
Vojtech Juranek <vjuranek@redhat.com>
2014-03-12 00:03:03 +0100

    Export ID of created container as a property

--- src/main/java/cz/jurankovi/docker/maven/ContainerCreateMojo.java
+++ src/main/java/cz/jurankovi/docker/maven/ContainerCreateMojo.java
@@ -1,9 +1,11 @@
 package cz.jurankovi.docker.maven;
 
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import com.kpelykh.docker.client.DockerClient;
 import com.kpelykh.docker.client.DockerException;
@@ -13,11 +15,16 @@ import com.kpelykh.docker.client.model.ContainerCreateResponse;
 @Mojo(name = "container-create", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
 public class ContainerCreateMojo extends AbstractDockerMojo {
 
+    public static final String PROPERTY_CONTAINER_ID = "__docker_container_id__"; 
+    
     @Parameter(required = true)
     private String imageId;
     @Parameter(required = true)
     private String command;
     private String hostName;
+    
+    @Component
+    private MavenProject project;
 
     public void execute() throws MojoExecutionException {
         try {
@@ -29,9 +36,14 @@ public class ContainerCreateMojo extends AbstractDockerMojo {
             DockerClient client = getClient();
             ContainerCreateResponse resp = client.createContainer(cfg);
             //TODO log response
+            exportContainerId(resp.getId());
         } catch (DockerException e) {
             // TODO log
             throw new MojoExecutionException(e.getMessage());
         }
+    }
+    
+    private void exportContainerId(String containerId) {
+        project.getProperties().put(PROPERTY_CONTAINER_ID, containerId);
     }
 }
