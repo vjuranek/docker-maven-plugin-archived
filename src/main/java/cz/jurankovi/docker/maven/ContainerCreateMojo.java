510a1b6f895aa0b85a1ea690f606d839803ac803
Vojtech Juranek <vjuranek@redhat.com>
2014-03-15 01:55:57 +0100

    Update ContainerCreateMojo to new base class

--- src/main/java/cz/jurankovi/docker/maven/ContainerCreateMojo.java
+++ src/main/java/cz/jurankovi/docker/maven/ContainerCreateMojo.java
@@ -1,6 +1,5 @@
 package cz.jurankovi.docker.maven;
 
-import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -15,34 +14,31 @@ import com.kpelykh.docker.client.model.ContainerCreateResponse;
 @Mojo(name = "container-create", defaultPhase = LifecyclePhase.PRE_INTEGRATION_TEST)
 public class ContainerCreateMojo extends AbstractDockerMojo {
 
-    public static final String PROPERTY_CONTAINER_ID = "__docker_container_id__"; 
-    
+    public static final String PROPERTY_CONTAINER_ID = "__docker_container_id__";
+
     @Parameter(required = true)
     private String imageId;
     @Parameter(required = true)
     private String command;
     private String hostName;
-    
+
     @Component
     private MavenProject project;
 
-    public void execute() throws MojoExecutionException {
-        try {
-            ContainerConfig cfg = new ContainerConfig();
-            cfg.setImage(imageId);
-            cfg.setCmd(new String[] { command });
-            if (hostName != null && !hostName.isEmpty())
-                cfg.setHostName(hostName);
-            DockerClient client = getClient();
-            ContainerCreateResponse resp = client.createContainer(cfg);
-            //TODO log response
-            exportContainerId(resp.getId());
-        } catch (DockerException e) {
-            // TODO log
-            throw new MojoExecutionException(e.getMessage());
-        }
+    public void doExecute() throws DockerException {
+
+        ContainerConfig cfg = new ContainerConfig();
+        cfg.setImage(imageId);
+        cfg.setCmd(new String[] { command });
+        if (hostName != null && !hostName.isEmpty())
+            cfg.setHostName(hostName);
+        DockerClient client = getClient();
+        ContainerCreateResponse resp = client.createContainer(cfg);
+        // TODO log response
+        exportContainerId(resp.getId());
+
     }
-    
+
     private void exportContainerId(String containerId) {
         project.getProperties().put(PROPERTY_CONTAINER_ID, containerId);
     }
